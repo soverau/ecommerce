985c083fda19bc705ea023ccda236a9a1bdab9ff
Vicky Oehm <vickyoehm@yahoo.com.ar>
2024-12-17 23:36:33 -0300

    Test 2

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const productosRoutes = require("./app/productos");
-const carritoRoutes = require("./app/carrito");
+const productManager = require("./app/productos");
+const cartManager = require("./app/carrito");
 
-app.use("./app/productos", productosRoutes);
-app.use("./app/carrito", carritoRoutes);
+app.use("./app/productos", productManager);
+app.use("./app/carrito", cartManager);
 
 const PORT = 8080;
 app.listen(PORT, () => {
